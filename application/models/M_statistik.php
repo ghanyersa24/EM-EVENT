b00c79b03dd558b24bdc586b0a86aa47c8f11de3
Ghany Abdillah Ersa <ghanyersa24@gmail.com>
2019-09-17 19:34:17 +0700

    statistik

--- application/models/M_statistik.php
+++ application/models/M_statistik.php
@@ -7,14 +7,11 @@ class M_statistik extends CI_Model
 		parent::__construct();
 		$this->load->database();
 	}
-	public function get( $idagenda)
+	public function get($idagenda)
 	{
-		$query = $this->db->select('TB_JADWAL.JADWAL,COUNT(NIM) JUMLAH, TB_DAFTAR.STATUS')
-						  ->from('TB_DAFTAR')
-						  ->join('TB_JADWAL', 'TB_DAFTAR.ID_C_JADWAL = TB_JADWAL.ID_C_JADWAL')
-						  ->where(array('TB_DAFTAR.ID_AGENDA' => $idagenda))
-						  ->group_by(array('TB_DAFTAR.ID_C_JADWAL','TB_DAFTAR.STATUS'))
-						  ->get()->result_array();
+		$query = $this->db->select('')
+			->from('TB')
+			->get()->result_array();
 		return $query;
 	}
 }
