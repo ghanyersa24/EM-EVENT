a5d85d52452337f8d2c408858abd140c095a4ce9
emub <em@ub.ac.id>
2019-09-18 06:54:58 +0700

    Login + Agenda done

--- application/views/Login.php
+++ application/views/Login.php
@@ -8,7 +8,7 @@
   <meta name="msapplication-tap-highlight" content="no">
   <meta name="description" content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google. ">
   <meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template,">
-  <title>Login Page | Materialize - Material Design Admin Template</title>
+  <title>EM-EVENT | Login</title>
   <link rel="apple-touch-icon-precomposed" href="images/favicon/apple-touch-icon-152x152.png">
   <meta name="msapplication-TileColor" content="#00bcd4">
   <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
@@ -27,49 +27,78 @@
       <form class="login-form">
         <div class="row">
           <div class="input-field col s12 center">
-            <img src="<?= base_url('assets/') ?>images/login-logo.png" alt="" class="circle responsive-img valign profile-image-login">
-            <p class="center login-form-text">Material Design Admin Template</p>
+            <img src="<?= base_url('assets/') ?>images/logo2.png" alt="" class="circle responsive-img valign profile-image-login">
+            <p class="center login-form-text">EM-EVENT Admin Dashboard</p>
           </div>
         </div>
         <div class="row margin">
           <div class="input-field col s12">
             <i class="mdi-social-person-outline prefix"></i>
-            <input id="username" type="text">
-            <label for="username" class="center-align">Username</label>
+            <input id="username" type="text" name="nim">
+            <label for="username" class="center-align">NIM</label>
           </div>
         </div>
         <div class="row margin">
           <div class="input-field col s12">
             <i class="mdi-action-lock-outline prefix"></i>
-            <input id="password" type="password">
+            <input id="password" type="password" name="password">
             <label for="password">Password</label>
           </div>
         </div>
-        <div class="row">
+        <!-- <div class="row">
           <div class="input-field col s12 m12 l12  login-text">
             <input type="checkbox" id="remember-me" />
             <label for="remember-me">Remember me</label>
           </div>
-        </div>
+        </div> -->
         <div class="row">
           <div class="input-field col s12">
-            <a href="index.html" class="btn waves-effect waves-light col s12">Login</a>
+            <a href="index.html" class="btn waves-effect waves-light col s12 cyan" onclick="login()">Login</a>
           </div>
         </div>
-        <div class="row">
+        <!-- <div class="row">
           <div class="input-field col s6 m6 l6">
             <p class="margin medium-small"><a href="page-register.html">Register Now!</a></p>
           </div>
           <div class="input-field col s6 m6 l6">
             <p class="margin right-align medium-small"><a href="page-forgot-password.html">Forgot password ?</a></p>
           </div>
-        </div>
+        </div> -->
 
       </form>
     </div>
   </div>
 
   <script type="text/javascript" src="<?= base_url('assets/js/') ?>materialize.js"></script>
+  
+  <script>
+    function login(){
+      event.preventDefault();
+      let nim = $('input[name="nim"]').val();
+      let password = $('input[name="password"]').val();
+      $.ajax({
+        url:
+        "https://em.ub.ac.id/redirect/login/loginApps/?nim=" +
+        nim +
+        "&password=" +
+        password +
+        "",
+        type: 'POST',
+        dataType:'json',
+        success:(r)=>{
+          if(r.status==true){
+            sessionStorage.setItem("nim",r.nim);
+            sessionStorage.setItem("nama",r.nama);
+            sessionStorage.setItem("fak",r.fak);
+            sessionStorage.setItem("prodi",r.prodi);
+            sessionStorage.setItem("jurusan",r.jurusan);
+            sessionStorage.setItem("foto",r.foto);
+            window.location.replace('<?=base_url('agenda')?>');
+          }
+        }
+      })
+    }
+  </script>
 </body>
 
 </html>
