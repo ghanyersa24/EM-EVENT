78e99902644d921dddb92931d0ae91e8a86c12e3
emub <em@ub.ac.id>
2019-10-13 06:44:03 +0700

    Query getAgenda

--- application/models/M_agenda.php
+++ application/models/M_agenda.php
@@ -9,7 +9,7 @@ class M_agenda extends CI_Model
 	}
 	public function getAgenda($nim)
 	{
-		$query = $this->db->query("SELECT DISTINCT `TB_AGENDA`.`ID_AGENDA`,`TB_AGENDA`.`NIM`,`TB_AGENDA`.`TB_AGENDA`,	`TB_AGENDA`.`FOTO`,	`TB_AGENDA`.`DESKRIPSI`	FROM `TB_PENGURUS` JOIN `TB_PILIHAN` ON `TB_PENGURUS`.`ID_PILIHAN` = `TB_PILIHAN`.`ID_PILIHAN` JOIN `TB_AGENDA` ON `TB_PILIHAN`.`ID_AGENDA` = `TB_AGENDA`.`ID_AGENDA` WHERE`TB_PENGURUS`.`NIM` = $nim OR `TB_AGENDA`.`NIM` = $nim")->result_array();
+		$query = $this->db->query("SELECT DISTINCT `TB_AGENDA`.`ID_AGENDA`,`TB_AGENDA`.`NIM`,`TB_AGENDA`.`TB_AGENDA`,	`TB_AGENDA`.`FOTO`,	`TB_AGENDA`.`DESKRIPSI`	FROM `TB_PENGURUS` JOIN `TB_PILIHAN` ON `TB_PENGURUS`.`ID_PILIHAN` = `TB_PILIHAN`.`ID_PILIHAN` JOIN `TB_AGENDA` ON `TB_PILIHAN`.`ID_AGENDA` = `TB_AGENDA`.`ID_AGENDA` WHERE `TB_PENGURUS`.`NIM` = '$nim' OR `TB_AGENDA`.`NIM` = '$nim'")->result_array();
 		return $query;
 	}
 	function check($id)
